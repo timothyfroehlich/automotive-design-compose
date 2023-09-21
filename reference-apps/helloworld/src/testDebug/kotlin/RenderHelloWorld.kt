fd19c2483cbc9919df89a9f8e74b3884c140ac48
Tim Froehlich <froeht@google.com>
2023-09-21 10:43:18 -0600

    Test with a change

--- reference-apps/helloworld/src/testDebug/kotlin/RenderHelloWorld.kt
+++ reference-apps/helloworld/src/testDebug/kotlin/RenderHelloWorld.kt
@@ -61,9 +61,9 @@ class RenderHelloWorld {
     @Test
     fun testHello() {
         with(composeTestRule) {
-            setContent { HelloWorldDoc.mainFrame(name = "I changed again!") }
+            setContent { HelloWorldDoc.mainFrame(name = "Tester!") }
             onNode(SemanticsMatcher.expectValue(docIdSemanticsKey, helloWorldDocId)).assertExists()
-            onNodeWithText("I changed again!", substring = true).assertExists()
+            onNodeWithText("Tester!", substring = true).assertExists()
         }
     }
 }
